4d661b551a86f10df3720d7307b1c284c109a0bc
Pabodha <pabodhap@gmail.com>
2023-05-25 01:29:53 +0530

    Globals styles added to glbals.css

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import styles from "./index.module.css";
-import { ImHome2 } from "react-icons/im";
-import { CgProfile } from "react-icons/cg";
-import { FiBarChart2 } from "react-icons/fi";
-import { RiDashboardLine } from "react-icons/ri";
-import { BsFillChatDotsFill } from "react-icons/bs";
-import { MdOutlineNotifications } from "react-icons/md";
+import { MdSpaceDashboard } from "react-icons/md";
+import { AiFillCaretLeft, AiFillHome } from "react-icons/ai";
+import { TbChartPieFilled } from "react-icons/tb";
+import { AiFillMessage } from "react-icons/ai";
+import { IoLogOut } from "react-icons/io5";
 
 const Sidebar = ({ children }: any) => {
+  const router = useRouter();
+  const routePath = router.asPath;
+  const [pageSelectArrow, setPageSelectArrow] = useState(true);
+
   return (
     <div className="flex">
       <div className={styles.sidebarDiv}>
@@ -24,37 +28,58 @@ const Sidebar = ({ children }: any) => {
               />
             </Link>
           </div>
-          <div className={styles.itemDiv}>
-            <Link href="/">
-              <RiDashboardLine className={styles.icon} />
+          <div className={styles.itemDiv} 
+            onMouseOver={() => setPageSelectArrow(false)} 
+            onMouseLeave={() => setPageSelectArrow(true)}
+          >
+            <Link href="/" className={styles.link}>
+              <MdSpaceDashboard className={styles.icon} />
             </Link>
+            <AiFillCaretLeft 
+              className={ routePath !== "/" ? styles.selectArrow : pageSelectArrow ? styles.selectArrowSelected : styles.selectArrow } 
+            />
           </div>
-          <div className={styles.itemDiv}>
-            <Link href="/">
-              <ImHome2 className={styles.icon} />
+          <div className={styles.itemDiv} 
+            onMouseOver={() => setPageSelectArrow(false)} 
+            onMouseLeave={() => setPageSelectArrow(true)}
+          >
+            <Link href="/home" className={styles.link}>
+              <AiFillHome className={styles.icon} />
             </Link>
+            <AiFillCaretLeft 
+              className={ routePath !== "/home" ? styles.selectArrow : pageSelectArrow ? styles.selectArrowSelected : styles.selectArrow } 
+            />
           </div>
-          <div className={styles.itemDiv}>
-            <Link href="/notification">
-              <MdOutlineNotifications className={styles.icon} />
+          <div className={styles.itemDiv} 
+            onMouseOver={() => setPageSelectArrow(false)} 
+            onMouseLeave={() => setPageSelectArrow(true)}
+          >
+            <Link href="/analytics" className={styles.link}>
+              <TbChartPieFilled className={styles.icon} />
             </Link>
+            <AiFillCaretLeft 
+              className={ routePath !== "/analytics" ? styles.selectArrow : pageSelectArrow ? styles.selectArrowSelected : styles.selectArrow } 
+            />
           </div>
-          <div className={styles.itemDiv}>
-            <Link href="/">
-              <FiBarChart2 className={styles.icon} />
+          <div className={styles.itemDiv} 
+            onMouseOver={() => setPageSelectArrow(false)} 
+            onMouseLeave={() => setPageSelectArrow(true)}
+          >
+            <Link href="/chat" className={styles.link}>
+              <AiFillMessage className={styles.icon} />
             </Link>
-          </div>
-          <div className={styles.itemDiv}>
-            <Link href="/profile">
-              <CgProfile className={styles.icon} />
-            </Link>
-          </div>
-          <div className={styles.itemDiv}>
-            <Link href="/chat">
-              <BsFillChatDotsFill className={styles.icon} />
-            </Link>
+            <AiFillCaretLeft 
+              className={ routePath !== "/chat" ? styles.selectArrow : pageSelectArrow ? styles.selectArrowSelected : styles.selectArrow } 
+            />
           </div>
         </div>
+
+
+        {/* Logout button */}
+        <div className={styles.logoutBtn}>
+          <IoLogOut className={styles.icon} />
+        </div>
+        
       </div>
 
       <main className={styles.childrenDiv}>{children}</main>
