0435b0a03d3b738177864d6f5c8da67c9383d19b
Sandunika Hathnapitiya <sandunikahathnapitiya@gmail.com>
2023-06-20 09:21:27 +0530

    Added edit-profile and change-password UIs

---
+++ pages/change-password/index.tsx
@@ -0,0 +1,31 @@
+
+import React from 'react'
+import Head from 'next/head';
+import type { Page } from "../../tsc-types/next";
+import Header from '@/components/LoginPages/Header';
+import ChangePasswordForm from '@/components/LoginPages/ChangePasswordForm';
+  // Props type
+  type Props = {
+    Component: Page;
+  };
+
+  export default function ChangePassword() {
+    return (
+      <>
+        <Head>
+          <title>Engage Change Password</title>
+          <meta name="description" content="" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className="bg-color min-h-screen">
+          <Header subTopic="Welcome to Engage" />
+          <ChangePasswordForm/>
+        </main>
+      </>
+    );
+  }
+
+  ChangePassword.getLayout = function pageLayout(page: Props) {
+    return <>{page}</>;
+  };
