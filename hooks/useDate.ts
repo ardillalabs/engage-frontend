84b24c5b580d1c56e5e3b515667f0a8c964cc5da
Pabodha <pabodhap@gmail.com>
2023-06-29 12:32:43 +0530

    daily message API

--- hooks/useDate.ts
+++ hooks/useDate.ts
@@ -1,25 +1,53 @@
-import React, { useMemo } from 'react'
+import React, { useMemo } from "react";
 
 const useDate = () => {
-    const date = new Date();
-    const weekdayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    const monthNames = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-    
-    const day = date.getDate();
-    const dayWithSyntax = day === 1 ? '1st' : day === 2 ? '2nd' : day === 3 ? '3rd' : `${day}th`;
-    const weekday = weekdayNames[date.getDay()];
-    const month = monthNames[date.getMonth()];
-    const year = date.getFullYear();
-    const dateString = date.toDateString();
+  const date = new Date();
+  const weekdayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-    return useMemo(() => ({
-        dateString,
-        day,
-        dayWithSyntax,
-        weekday,
-        month,
-        year
-    }),[dateString,day, dayWithSyntax, weekday, month, year]);
-}
+  const day = date.getDate();
+  const dayWithSyntax =
+    day === 1 ? "1st" : day === 2 ? "2nd" : day === 3 ? "3rd" : `${day}th`;
+  const weekday = weekdayNames[date.getDay()];
+  const month = monthNames[date.getMonth()];
+  const monthNum =
+    date.getMonth() >= 10 ? date.getMonth() : `0${date.getMonth()}`;
+  const year = date.getFullYear();
+  const dateString = date.toDateString();
+
+  return useMemo(
+    () => ({
+      dateString,
+      day,
+      dayWithSyntax,
+      weekday,
+      month,
+      monthNum,
+      year,
+    }),
+    [dateString, day, dayWithSyntax, weekday, month, monthNum, year]
+  );
+};
 
 export default useDate;
