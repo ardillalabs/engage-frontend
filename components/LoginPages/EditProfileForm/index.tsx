722bd5b44d3daf9bbfad9d4609d35ab3e33a2968
kavishkaG <kavishkaganepola1995@gmail.com>
2023-07-01 20:19:45 +0530

    Update index.tsx

--- components/LoginPages/EditProfileForm/index.tsx
+++ components/LoginPages/EditProfileForm/index.tsx
@@ -1,33 +1,41 @@
-import React, { useEffect, useRef, useState } from 'react'
-import Link from "next/link";
-import styles from "./index.module.css";
-import { UserInforUpdateDetails, UserProfileUpdateDetails } from '@/tsc-types/Auth';
+import React, { useEffect, useRef, useState } from 'react';
+import Link from 'next/link';
+import styles from './index.module.css';
+import {
+  UserInforUpdateDetails,
+  UserProfileUpdateDetails,
+} from '@/tsc-types/Auth';
 
 //redux
 import { connect } from 'react-redux';
 import { RootState } from '@/store';
-import PropTypes from "prop-types";
-import { getCurrentUserDetails, getProfileDetails, updateUserDetailsSubmit } from '@/actions/Auth';
+import PropTypes from 'prop-types';
+import {
+  getCurrentUserDetails,
+  getProfileDetails,
+  updateUserDetailsSubmit,
+} from '@/actions/Auth';
 import { useCookies } from 'react-cookie';
 import { useRouter } from 'next/router';
 
 interface Props {
-  updateProfile: UserProfileUpdateDetails;
-  userDetails: UserInforUpdateDetails;
   getProfileDetails: (...args: any[]) => any;
   updateUserDetailsSubmit: (...args: any[]) => any;
-  auth: UserProfileUpdateDetails
+  auth: UserProfileUpdateDetails;
 }
 
-const EditProfileForm = ({ getProfileDetails, updateUserDetailsSubmit, auth }: Props) => {
+const EditProfileForm = ({
+  getProfileDetails,
+  updateUserDetailsSubmit,
+  auth,
+}: Props) => {
   const router = useRouter();
 
   const myRef = useRef<any>({});
 
   useEffect(() => {
     getProfileDetails('cookies.access_token');
-}, [getProfileDetails]);
-
+  }, [getProfileDetails]);
 
   const [isClick, setClick] = useState({
     Name: false,
@@ -38,18 +46,18 @@ const EditProfileForm = ({ getProfileDetails, updateUserDetailsSubmit, auth }: P
   const [isData, setData] = useState({
     Name: '',
     UserName: '',
-    PhoneNumber: ''
+    PhoneNumber: '',
   });
 
   const [errors, setErrors] = useState({
-    Name: "",
-    UserName: "",
-    PhoneNumber: ""
+    Name: '',
+    UserName: '',
+    PhoneNumber: '',
   });
 
   useEffect(() => {
     if (auth) {
-      setData(prevData => ({
+      setData((prevData) => ({
         ...prevData,
         Name: auth.full_name,
         UserName: auth.username,
@@ -58,7 +66,6 @@ const EditProfileForm = ({ getProfileDetails, updateUserDetailsSubmit, auth }: P
     }
   }, [auth]);
 
-  
   const handleChange = (values: any) => {
     setData({
       ...isData,
@@ -67,51 +74,58 @@ const EditProfileForm = ({ getProfileDetails, updateUserDetailsSubmit, auth }: P
   };
 
   useEffect(() => {
-     if(isData.UserName && isData.UserName.length === 0) {
-      setErrors({ ...errors, UserName: "" });
+    if (isData.UserName && isData.UserName.length === 0) {
+      setErrors({ ...errors, UserName: '' });
     }
   }, [isData.UserName]);
 
   useEffect(() => {
-      if(isData.PhoneNumber && isData.PhoneNumber.length === 0) {
-      setErrors({ ...errors, PhoneNumber: "" });
+    if (isData.PhoneNumber && isData.PhoneNumber.length === 0) {
+      setErrors({ ...errors, PhoneNumber: '' });
     }
   }, [isData.PhoneNumber]);
-  
+
   const UpdateProfileDetailsSubmit = () => {
     const errors = {
-      Name: "",
-      UserName: "",
-      PhoneNumber: ""
+      Name: '',
+      UserName: '',
+      PhoneNumber: '',
     };
 
     if (!isData.Name) {
-      errors.Name = "The field is required";
+      errors.Name = 'The field is required';
     }
-  
+
     if (isData.Name && isData.Name.length < 2) {
-      errors.Name = "Min length: 2";
+      errors.Name = 'Min length: 2';
     }
-  
+
     if (!isData.UserName) {
-      errors.UserName = "The field is required";
+      errors.UserName = 'The field is required';
     }
-  
+
     if (isData.UserName && isData.UserName.length < 2) {
-      errors.UserName = "Min length: 2";
+      errors.UserName = 'Min length: 2';
     }
 
     if (!isData.PhoneNumber) {
-      errors.PhoneNumber = "The field is required";
+      errors.PhoneNumber = 'The field is required';
     } else if (
       isData.PhoneNumber.length > 13 ||
       isData.PhoneNumber.length < 10
     ) {
-      errors.PhoneNumber = "Phone number is invalid.";
+      errors.PhoneNumber = 'Phone number is invalid.';
     }
 
     if (!(errors.Name || errors.UserName || errors.PhoneNumber)) {
-      updateUserDetailsSubmit({ full_name: isData.Name, username: isData.UserName, phone_number: isData.PhoneNumber }, 'cookies.access_token');
+      updateUserDetailsSubmit(
+        {
+          full_name: isData.Name,
+          username: isData.UserName,
+          phone_number: isData.PhoneNumber,
+        },
+        'cookies.access_token'
+      );
     }
 
     setErrors(errors);
@@ -119,48 +133,48 @@ const EditProfileForm = ({ getProfileDetails, updateUserDetailsSubmit, auth }: P
 
   useEffect(() => {
     if (auth.isUpdatedUserInfo) {
-      router.push("/dashboard");
+      router.push('/dashboard');
     }
   }, [auth]);
 
   return (
     <div className={styles.mainDiv}>
       <div className={styles.componentDiv}>
-      <div className={styles.headerDiv}>
-           <h2>Edit Profile</h2> 
+        <div className={styles.headerDiv}>
+          <h2>Edit Profile</h2>
         </div>
         <div className={styles.inputDiv}>
           <div>Full Name</div>
           <input
-            type="text"
+            type='text'
             className={styles.input}
-            id="name"
+            id='name'
             value={isData.Name}
             autoFocus
             ref={(input) => (myRef.current.name = input)}
             onChange={(e) => handleChange({ Name: e.target.value })}
           />
-           <div className={styles.errorMessage}>{errors.Name}</div>
+          <div className={styles.errorMessage}>{errors.Name}</div>
         </div>
         <div className={styles.inputDiv}>
           <div>User Name</div>
           <input
-            type="text"
+            type='text'
             className={styles.input}
-            id="username"
+            id='username'
             value={isData.UserName}
             autoFocus
             ref={(input) => (myRef.current.username = input)}
             onChange={(e) => handleChange({ UserName: e.target.value })}
           />
-           <div className={styles.errorMessage}>{errors.UserName}</div>
+          <div className={styles.errorMessage}>{errors.UserName}</div>
         </div>
         <div className={styles.inputDiv}>
           <div>Phone Number</div>
           <input
-            type="text"
+            type='text'
             className={styles.input}
-            id="phonenumber"
+            id='phonenumber'
             value={isData.PhoneNumber}
             autoFocus
             ref={(input) => (myRef.current.phonenumber = input)}
@@ -168,19 +182,22 @@ const EditProfileForm = ({ getProfileDetails, updateUserDetailsSubmit, auth }: P
           />
           <div className={styles.errorMessage}>{errors.PhoneNumber}</div>
         </div>
-       
-        <div className={styles.buttonDiv}  onClick={() => UpdateProfileDetailsSubmit()}>
-            <button>Update</button>
+
+        <div
+          className={styles.buttonDiv}
+          onClick={() => UpdateProfileDetailsSubmit()}
+        >
+          <button>Update</button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 // export default EditProfileForm
 EditProfileForm.propTypes = {
   getProfileDetails: PropTypes.func.isRequired,
-  updateUserDetailsSubmit: PropTypes.func.isRequired
+  updateUserDetailsSubmit: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state: RootState) => ({
@@ -188,5 +205,6 @@ const mapStateToProps = (state: RootState) => ({
 });
 
 export default connect(mapStateToProps, {
-  getProfileDetails, updateUserDetailsSubmit
+  getProfileDetails,
+  updateUserDetailsSubmit,
 })(EditProfileForm);
