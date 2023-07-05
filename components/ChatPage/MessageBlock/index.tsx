919cb025a6031edfc1fc29eeff59a89e4d9da7ac
Pabodha <pabodhap@gmail.com>
2023-07-05 11:45:39 +0530

    Support member data fetch

--- components/ChatPage/MessageBlock/index.tsx
+++ components/ChatPage/MessageBlock/index.tsx
@@ -1,25 +1,28 @@
 import Image from "next/image";
 import styles from "./index.module.css";
 
-const MessageBlock = ({reversed, messageTime}: any) => {
-    return (
-        <div className={styles.mainDiv}>
-            <div className={ !reversed ? styles.profileInfo : styles.profileInfoReversed }>
-                <Image
-                        src="https://source.unsplash.com/_7LbC5J-jw4"
-                        alt="Profile Picture"
-                        className={styles.profilePicture}
-                        width={60}
-                        height={60}
-                />
-                <div className={styles.profileInfoText}>
-                    { !reversed && <div className="body-heading">Denneal Perera</div> }
-                    <div className={ !reversed ? styles.time : styles.timeReversed }>{messageTime}</div>
-                </div>
-            </div>
-
+const MessageBlock = ({ reversed, messageTime, username }: any) => {
+  return (
+    <div className={styles.mainDiv}>
+      <div
+        className={!reversed ? styles.profileInfo : styles.profileInfoReversed}
+      >
+        <Image
+          src="https://source.unsplash.com/_7LbC5J-jw4"
+          alt="Profile Picture"
+          className={styles.profilePicture}
+          width={60}
+          height={60}
+        />
+        <div className={styles.profileInfoText}>
+          {!reversed && <div className="body-heading">{username}</div>}
+          <div className={!reversed ? styles.time : styles.timeReversed}>
+            {messageTime}
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default MessageBlock;
