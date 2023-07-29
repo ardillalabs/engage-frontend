be3fb5151ad60b07028c61328b343b42c52968b5
Pabodha <pabodhap@gmail.com>
2023-07-29 15:06:47 +0530

    signup steps fix

--- components/LoginPages/SignUpSteps/index.tsx
+++ components/LoginPages/SignUpSteps/index.tsx
@@ -1,24 +1,34 @@
-import React, { useState } from 'react'
-import styles from './index.module.css'
+import React, { useState } from "react";
+import styles from "./index.module.css";
 
 interface prop {
-    step: string
+  step: string;
 }
 
-const SignUpSteps = ({step}: prop) => {
-    const[activeStep, setActiveStep] = useState<string>(step)
-    function changeStep(changedStep: string): any {
-        setActiveStep(changedStep)
-    }
+const SignUpSteps = ({ step }: prop) => {
+  const [activeStep, setActiveStep] = useState<string>(step);
+
   return (
     <div className={styles.stepsDiv}>
-        <div  className={activeStep === "1" ? styles.activeStep : styles.inactiveStep} onClick={() => changeStep("1")}>1</div>
-        <hr className={styles.lineDiv} />
-        <div className={activeStep === "2" ? styles.activeStep : styles.inactiveStep} onClick={() =>changeStep("2")}>2</div>
-        <hr className={styles.lineDiv} />
-        <div className={activeStep === "3" ? styles.activeStep : styles.inactiveStep} onClick={() => changeStep("3")}>3</div>
-  </div>
-  )
-}
+      <div
+        className={activeStep === "1" ? styles.activeStep : styles.inactiveStep}
+      >
+        1
+      </div>
+      <hr className={styles.lineDiv} />
+      <div
+        className={activeStep === "2" ? styles.activeStep : styles.inactiveStep}
+      >
+        2
+      </div>
+      <hr className={styles.lineDiv} />
+      <div
+        className={activeStep === "3" ? styles.activeStep : styles.inactiveStep}
+      >
+        3
+      </div>
+    </div>
+  );
+};
 
-export default SignUpSteps
+export default SignUpSteps;
