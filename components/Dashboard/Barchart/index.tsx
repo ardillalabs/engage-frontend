5c935fccc3b13fd839eaafc621ff2a999120d760
kavishkaG <kavishkaganepola1995@gmail.com>
2023-09-01 06:41:31 +0530

    Update index.tsx

--- components/Dashboard/Barchart/index.tsx
+++ components/Dashboard/Barchart/index.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef, useState } from "react";
-import type { ChartArea } from "chart.js";
-import PropTypes from "prop-types";
-import styles from "./index.module.css";
+import React, { useEffect, useRef, useState } from 'react';
+import type { ChartArea } from 'chart.js';
+import PropTypes from 'prop-types';
+import styles from './index.module.css';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -10,12 +10,12 @@ import {
   Title,
   Tooltip,
   Legend,
-} from "chart.js";
-import { Bar } from "react-chartjs-2";
-import useDate from "@/hooks/useDate";
-import { RootState } from "@/store";
-import { connect } from "react-redux";
-import { FaDownload } from "react-icons/fa";
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import useDate from '@/hooks/useDate';
+import { RootState } from '@/store';
+import { connect } from 'react-redux';
+import { FaDownload } from 'react-icons/fa';
 
 const BASE_URL = process.env.BASE_URL;
 
@@ -81,7 +81,7 @@ const Barchart = ({ auth }: Props) => {
       },
       title: {
         display: false,
-        text: "Chart.js Bar Chart",
+        text: 'Chart.js Bar Chart',
       },
     },
   };
@@ -90,9 +90,9 @@ const Barchart = ({ auth }: Props) => {
   const [dailyScores, setDailyScores] = useState<dailyScores[]>();
   const [dailyScoreLabels, setDailyScoreLabels] = useState<any>();
   const { day, monthNum, year } = useDate();
-  console.log(year, monthNum, day)
-  
-  const date = new Date(); 
+  console.log(year, monthNum, day);
+
+  const date = new Date();
 
   date.setDate(date.getDate() - 7);
   console.log(date);
@@ -106,17 +106,17 @@ const Barchart = ({ auth }: Props) => {
   const formattedDate = `${yearStart}-${monthStart}-${dayStart}`;
   useEffect(() => {
     const date = new Date();
-    console.log(date)
-    const dailyLabels = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    console.log(date);
+    const dailyLabels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     const DailyDataFetch = async () => {
       try {
         const res = fetch(
           // `${BASE_URL}/quiz_mark/last-7-day-summery/DAY/2/${auth.id}`
-          `http://localhost:5000/api/quiz_mark/get-marks-date-range/DAY/2/${auth.id}/${formattedDate}/${year}-${monthNum}-${day}`
+          `https://api.stayengaged.io/api/quiz_mark/get-marks-date-range/DAY/2/${auth.id}/${formattedDate}/${year}-${monthNum}-${day}`
         );
         (await res).json().then((days: dailyScores[]) => {
           setDailyScores(days);
-          console.log(days)
+          console.log(days);
           // const tempDailyData = [];
 
           // for (let i = 0; i < days.length; i++) {
@@ -135,12 +135,17 @@ const Barchart = ({ auth }: Props) => {
   }, [auth.id]);
 
   const dailyData = {
-    labels: dailyScores?.map(day =>  new Date(day.day).toLocaleDateString('en-US', { month: 'short', day: 'numeric' })),
+    labels: dailyScores?.map((day) =>
+      new Date(day.day).toLocaleDateString('en-US', {
+        month: 'short',
+        day: 'numeric',
+      })
+    ),
     datasets: [
       {
         backgroundColor: (context: any) => {
           const { ctx, chartArea } = context.chart;
-          return createGradient(ctx, chartArea, "#1F2F98", "#2EAAFA");
+          return createGradient(ctx, chartArea, '#1F2F98', '#2EAAFA');
         },
         borderRadius: 6,
         data: Array.isArray(dailyScores)
@@ -154,10 +159,10 @@ const Barchart = ({ auth }: Props) => {
   const [weeklyScores, setWeeklyScores] = useState<weeklyScores[]>();
 
   const sevenWeeksAgo = new Date(date);
-  sevenWeeksAgo.setDate(date.getDate() - (7 * 7)); // Subtract 7 weeks
+  sevenWeeksAgo.setDate(date.getDate() - 7 * 7); // Subtract 7 weeks
 
   const weekObject = new Date(sevenWeeksAgo);
-  console.log(weekObject)
+  console.log(weekObject);
 
   const yearStart1 = weekObject.getFullYear();
   const monthStart1 = (weekObject.getMonth() + 1).toString().padStart(2, '0');
@@ -169,9 +174,9 @@ const Barchart = ({ auth }: Props) => {
       try {
         const res = fetch(
           // `${BASE_URL}/quiz_mark/last-7-day-summery/WEEK/1/${auth.id}`
-          `http://localhost:5000/api/quiz_mark/get-marks-date-range/WEEK/1/${auth.id}/${formattedWeek}/${year}-${monthNum}-${day}`
+          `https://api.stayengaged.io/api/quiz_mark/get-marks-date-range/WEEK/1/${auth.id}/${formattedWeek}/${year}-${monthNum}-${day}`
         );
-        console.log("last-7-day-summery/WEEK - res ", res);
+        console.log('last-7-day-summery/WEEK - res ', res);
         (await res).json().then((weeks) => {
           setWeeklyScores(weeks);
         });
@@ -183,12 +188,12 @@ const Barchart = ({ auth }: Props) => {
   }, [auth.id]);
 
   const weeklyData = {
-    labels: weeklyScores?.map(week =>  week.weekRange),
+    labels: weeklyScores?.map((week) => week.weekRange),
     datasets: [
       {
         backgroundColor: (context: any) => {
           const { ctx, chartArea } = context.chart;
-          return createGradient(ctx, chartArea, "#007542", "#B3F68A");
+          return createGradient(ctx, chartArea, '#007542', '#B3F68A');
         },
         borderRadius: 6,
         data: Array.isArray(weeklyScores)
@@ -203,8 +208,11 @@ const Barchart = ({ auth }: Props) => {
       <div className={styles.chartWrapper}>
         <div className={styles.leftDiv}>
           <div className={styles.chartHeader}>Daily Quiz Score</div>
-          
-        <button className={styles.downloadButtonDaily}><FaDownload className={styles.icon}/>Download as csv</button>
+
+          <button className={styles.downloadButtonDaily}>
+            <FaDownload className={styles.icon} />
+            Download as csv
+          </button>
         </div>
         {/* <div className={styles.rightDiv}>     */}
         {/* </div> */}
@@ -214,9 +222,12 @@ const Barchart = ({ auth }: Props) => {
         <Bar options={options} data={dailyData} />
       </div>
       <div className={styles.chartWrapper}>
-      <div className={styles.leftDiv}>
-        <div className={styles.chartHeader}>Weekly Quiz Score</div>
-        <button className={styles.downloadButtonWeekly}><FaDownload className={styles.icon}/>Download as csv</button>
+        <div className={styles.leftDiv}>
+          <div className={styles.chartHeader}>Weekly Quiz Score</div>
+          <button className={styles.downloadButtonWeekly}>
+            <FaDownload className={styles.icon} />
+            Download as csv
+          </button>
         </div>
         <div className={styles.chartSubHeader}>
           Productivity of the last 7 weeks
