165ed9bf12384d3a59da3d98059f71d2db3984cc
Pabodha <pabodhap@gmail.com>
2023-09-17 22:01:20 +0530

    API token

--- components/ChatPage/ChatBox/VideoRoom/index.tsx
+++ components/ChatPage/ChatBox/VideoRoom/index.tsx
@@ -4,8 +4,8 @@ import VideoPlayer from "../VideoPlayer";
 import styles from "./index.module.css";
 import { RootState } from "@/store";
 import { connect } from "react-redux";
-import { BsFillMicMuteFill } from "react-icons/bs";
-import { FaVideoSlash } from "react-icons/fa";
+import { BsFillMicFill, BsFillMicMuteFill } from "react-icons/bs";
+import { FaVideo, FaVideoSlash } from "react-icons/fa";
 import { HiOutlinePhoneMissedCall } from "react-icons/hi";
 import {
   addDoc,
@@ -17,6 +17,7 @@ import {
 } from "firebase/firestore";
 import { db } from "@/firebase/config";
 import { Router, useRouter } from "next/router";
+const BASE_URL = process.env.BASE_URL;
 
 const APP_ID = "98fd19afaf3248548277a8b98b20a34d";
 const TOKEN =
@@ -30,6 +31,8 @@ const client = AgoraRTC.createClient({
 
 const VideoRoom = ({ auth, leaveCall, recUserID }: any) => {
   const stateRef: any = useRef();
+  // const tokenRef: any = useRef();
+  // const [videoToken, setVideoToken] = useState();
   const [users, setUsers] = useState<any>([]);
   const [engageUsers, setEngageUsers] = useState<string[]>([]);
   const [videoAccess, setVideoAccess] = useState(false);
@@ -38,6 +41,24 @@ const VideoRoom = ({ auth, leaveCall, recUserID }: any) => {
   const chatID: any = router.query.chatID;
   const userID = auth.id;
   stateRef.current = users;
+  // tokenRef.current = videoToken;
+  console.log(auth);
+
+  // const tokenFetch = async () => {
+  //   const res: any = await fetch(
+  //     `${BASE_URL}/video-call/agora-access-token/${auth.id}/${chatID}`
+  //   );
+
+  //   const token = await res.json().then((tk: any) => {
+  //     // setVideoToken(tk.token);
+  //     // tokenRef.current = tk.token;
+  //     return tk.token;
+  //   });
+
+  //   return token
+  // };
+
+  // const videoToken = tokenFetch();
 
   const handleUserJoined = async (user: any, mediaType: any) => {
     await client.subscribe(user, mediaType);
@@ -90,7 +111,17 @@ const VideoRoom = ({ auth, leaveCall, recUserID }: any) => {
       setVideoAccess(true);
     }
   };
-  const muteAudio = () => {};
+  const muteAudio = async () => {
+    const index = users.findIndex((user: any) => user.uid === auth.id);
+
+    if (audioAccess) {
+      await users[index].tracks.audioTrack.setEnabled(false);
+      setAudioAccess(false);
+    } else {
+      await users[index].tracks.audioTrack.setEnabled(true);
+      setAudioAccess(true);
+    }
+  };
 
   const endVideo = async () => {
     client.leave();
@@ -133,30 +164,47 @@ const VideoRoom = ({ auth, leaveCall, recUserID }: any) => {
   };
 
   useEffect(() => {
-    client.on("user-published", handleUserJoined);
-    client.on("user-left", handleUserLeft);
+    const agoraClientPublish = async () => {
+      const res: any = await fetch(
+        `${BASE_URL}/video-call/agora-access-token/${auth.id}/${chatID}`
+      );
 
-    if (!engageUsers.includes(auth.id) && engageUsers.length < 2) {
-      client
-        .join(APP_ID, CHANNEL, TOKEN, auth.id)
-        .then((uid: any) =>
-          Promise.all([AgoraRTC.createMicrophoneAndCameraTracks(), uid])
-        )
-        .then(([tracks, uid]) => {
-          const [audioTrack, videoTrack] = tracks;
-          setUsers((prevUsers: any) => [
-            ...prevUsers,
-            {
-              uid,
-              tracks: { videoTrack, audioTrack },
-            },
-          ]);
-          setEngageUsers((prevUsers: any) => [...prevUsers, auth.id]);
-          client.publish(tracks);
-          setAudioAccess(true);
-          setVideoAccess(true);
-        });
-    }
+      const videoToken = await res.json().then((tk: any) => {
+        return tk.token;
+      });
+
+      console.log(videoToken);
+
+      client.on("user-published", handleUserJoined);
+      client.on("user-left", handleUserLeft);
+
+      if (!engageUsers.includes(auth.id) && engageUsers.length < 2) {
+        client
+          .join(APP_ID, chatID, videoToken, auth.id)
+          .then((uid: any) =>
+            Promise.all([AgoraRTC.createMicrophoneAndCameraTracks(), uid])
+          )
+          .then(([tracks, uid]) => {
+            const [audioTrack, videoTrack] = tracks;
+            setUsers((prevUsers: any) => [
+              ...prevUsers,
+              {
+                uid,
+                tracks: {
+                  videoTrack,
+                  audioTrack,
+                },
+              },
+            ]);
+            setEngageUsers((prevUsers: any) => [...prevUsers, auth.id]);
+            client.publish(tracks);
+            setAudioAccess(true);
+            setVideoAccess(true);
+          });
+      }
+    };
+
+    agoraClientPublish();
   }, []);
 
   console.log(users);
@@ -175,11 +223,11 @@ const VideoRoom = ({ auth, leaveCall, recUserID }: any) => {
           ))}
 
         <div className={styles.controls}>
-          <div>
-            <BsFillMicMuteFill />
+          <div onClick={() => muteAudio()}>
+            {audioAccess ? <BsFillMicMuteFill /> : <BsFillMicFill />}
           </div>
           <div onClick={() => muteVideo()}>
-            <FaVideoSlash />
+            {videoAccess ? <FaVideoSlash /> : <FaVideo />}
           </div>
           <div onClick={() => endVideo()}>
             <HiOutlinePhoneMissedCall />
