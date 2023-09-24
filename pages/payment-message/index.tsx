4b8b0e7266f7c379bb5bdc2a3a70ca1da39c2601
Pabodha <pabodhap@gmail.com>
2023-09-25 00:08:16 +0530

    bug-3 payment message UI added

--- pages/payment-message/index.tsx
+++ pages/payment-message/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import type { Page } from "../../tsc-types/next";
-import Head from "next/head";
-import PaymentMessage from "@/components/LoginPages/Payment/PaymentMessage";
+import React, { useState, useEffect } from 'react';
+import type { Page } from '../../tsc-types/next';
+import Head from 'next/head';
+import PaymentMessage from '@/components/LoginPages/Payment/PaymentMessage';
 
 type Props = {
   Component: Page;
@@ -18,13 +18,13 @@ export default function PaymentMessagePage() {
     <div>
       <Head>
         <title>Engage - Payment Message</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         {/* If we go live we want remove this meta tag 👇 */}
-        <meta name="robots" content="noindex, nofollow" />
-        <link rel="shortcut icon" href="./favicon.ico" />
-        <link rel="icon" type="image/x-icon" href="../favicon.ico" />
+        <meta name='robots' content='noindex, nofollow' />
+        <link rel='shortcut icon' href='./favicon.ico' />
+        <link rel='icon' type='image/x-icon' href='../favicon.ico' />
       </Head>
-      <main className="bg-color min-h-screen">
+      <main className='bg-color min-h-screen'>
         <PaymentMessage />
       </main>
     </div>
